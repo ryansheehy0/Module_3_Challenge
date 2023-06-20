b24974068504221564449f0ccda069f03577bf00
ryansheehy0 <ryansheehy0@gmail.com>
2023-06-19 21:37:23 -0700

    Adds password length.

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,20 @@
 // Assignment code here
 function generatePassword() {
+	let password_length = prompt("Password length from 8 to 128.");
+	let prompt_canceled = password_length === null ? true : false
+	password_length = parseInt(password_length)
+	while(prompt_canceled === false || Number.isNaN(password_length) || (password_length < 8) || (password_length > 128)){
+	console.log(password_length)
+		if(Number.isNaN(password_length)){
+			password_length = parseInt(prompt("Password length from 8 to 128.\nInput has to be a number."));
+		}else if((password_length < 8) || (password_length > 128)){
+			password_length = parseInt(prompt("Password length from 8 to 128.\nInput has to be from 8 to 128."));
+		}
+	}
 
+
+
+	return password_length
 }
 
 // Get references to the #generate element
